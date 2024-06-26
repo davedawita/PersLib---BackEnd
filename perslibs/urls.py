452d2282497dbb2878b8c2ed6c300854a036c284
Dawit <davedawita@yahoo.com>
2024-06-25 22:00:24 -0400

    urls setup & tested with postman

--- perslibs/urls.py
+++ perslibs/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from perslib.views import PerslibViewSet
+
+router = routers.DefaultRouter()
+router.register(r'perslib',PerslibViewSet) #register "/perslib" routes
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
