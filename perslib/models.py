1ddfd25b35e6dd961ec44dce42dbb1062500e4f6
Dawit <davedawita@yahoo.com>
2024-07-09 22:03:41 -0400

    Removed Login

--- perslib/models.py
+++ perslib/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 # from django.contrib.auth.models import AbstractBaseUser    #This is the base user in django.
 from django.db.models.signals import post_save
 # from django.contrib.auth.models import PermissionsMixin     #This allows us to set what users can and cannot do.
